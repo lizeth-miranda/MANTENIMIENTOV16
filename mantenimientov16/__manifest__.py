1bb75253a90e6fef3caa06ceaae94729682b4a62
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2024-06-21 16:49:42 -0600

    Update __manifest__.py

--- mantenimientov16/__manifest__.py
+++ mantenimientov16/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Mantenimiento Demsav16',
-    'version': '16.1',
+    'version': '16.2',
     'author': 'Demsa Industrial',
     'website': '',
     'depends': [
